0f433dc01e2aa1100d5e8c003a2f4b50efc46112
yavuzyagis <m.yavuzyagis@gmail.com>
2023-02-23 12:41:32 +0300

    Fix some bugs and add some features. This is an AUTOMATED Commit

--- go.mod
+++ go.mod
@@ -2,9 +2,10 @@ module github.com/MYavuzYAGIS/Poker
 
 go 1.19
 
+require github.com/fatih/color v1.14.1
+
 require (
-	github.com/fatih/color v1.14.1 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
-	golang.org/x/sys v0.3.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
 )
--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/fatih/color"
 )
 
 func main() {
@@ -12,8 +14,6 @@ func main() {
 
 	url := flag.String("url", "Please provide a valid URL without http or https", "127.0.0.1")
 	flag.Parse()
-	fmt.Println("url", *url)
-
 	connect(url)
 }
 
@@ -21,7 +21,20 @@ func connect(url *string) {
 	resp, err := http.Get(*url)
 	if err != nil {
 		log.Fatalln(err)
+		color.Red("Cannot connect to the given URL")
+		return
 	}
-	fmt.Println("connected to ==> ", *url)
+	defer resp.Body.Close()
+	fmt.Println(resp.StatusCode)
+	statusOK := resp.StatusCode >= 200 && resp.StatusCode < 400
+	if !statusOK {
+		fmt.Println("Non-OK HTTP status:", resp.StatusCode)
+		return
+	}
+	color.Blue("connected to ==> ", *url)
+	fmt.Println("\n", "====================================")
 	fmt.Println(resp)
+	fmt.Println("\n", "====================================")
 }
+
+// TODO: if url is given without a scheme, try adding scheme(http://) or (https://)
