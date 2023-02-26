4aadbd3e2f3f30d0789be3724e7d69fca435c50f
yavuzyagis <m.yavuzyagis@gmail.com>
2023-02-27 00:36:51 +0300

    Fix some bugs and add some features. This is an AUTOMATED Commit

--- helpers/banner.go
+++ helpers/banner.go
@@ -8,5 +8,4 @@ func Printbanner() {
 	fmt.Println("\t O--o  o-o OO   o-o o-o ")
 	fmt.Println("\t |     | | | \\  |-' |")
 	fmt.Println("\t o     o-o o  o o-o o ")
-	fmt.Println("\n")
 }
--- utils/cookie.go
+++ utils/cookie.go
@@ -7,14 +7,14 @@ import (
 )
 
 func Cookie(url string) {
-	color.Green("Checking for cookies. \n")
+	color.Green("Checking for cookies.")
 	fmt.Println("=======================")
 	resp := Connect(url)
 	cookies := resp.Cookies()
 	if len(cookies) == 0 {
 		fmt.Println("No Cookies detected")
 	} else {
-		fmt.Println("cookies:\n")
+		fmt.Println("cookies")
 		for _, cookie := range resp.Cookies() {
 			color.Green("Found a cookie named: %v, %v", cookie.Name, cookie.Value)
 		}
