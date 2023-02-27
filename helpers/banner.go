5fcafe353438c27414f13671f648d307b0a91dd1
yavuzyagis <m.yavuzyagis@gmail.com>
2023-02-27 13:53:27 +0300

    tweak interesting keywords, tweak banner

--- helpers/banner.go
+++ helpers/banner.go
@@ -1,6 +1,11 @@
 package helpers
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+
+	"github.com/fatih/color"
+)
 
 func Printbanner() {
 	fmt.Println("\t o--o      o  ")
@@ -8,4 +13,6 @@ func Printbanner() {
 	fmt.Println("\t O--o  o-o OO   o-o o-o ")
 	fmt.Println("\t |     | | | \\  |-' |")
 	fmt.Println("\t o     o-o o  o o-o o ")
+	color.Red("Automates trivial enumeration \t https://github.com/MYavuzYAGIS")
+	time.Sleep(1 * time.Second)
 }
